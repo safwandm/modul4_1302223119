d7bce5bdae14cb251742c3b0acad9aa9ad82923d
safwandm <35037131+safwandm@users.noreply.github.com>
2024-03-16 09:07:33 +0700

    menambahkan kelas PosisiKarakterGame

---
+++ modul3_1302223119/PosisiKarakterGame.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static modul4_1302223119.PosisiKarakterGame;
+
+namespace modul4_1302223119
+{
+    internal class PosisiKarakterGame
+    {
+        public enum State { Jongkok, Berdiri, Tengkurap, Terbang };
+
+        public enum Trigger { TombolW, TombolS, TombolX }
+
+        public State prevState, nextState;
+
+        public State currentState;
+
+        public Trigger trigger;
+
+        public PosisiKarakterGame(State currentState)
+        {
+            this.currentState = currentState;
+        }
+
+        public PosisiKarakterGame(State prevState, State nextState, Trigger trigger)
+        {
+            this.prevState = prevState;
+            this.nextState = nextState;
+            this.trigger = trigger;
+        }
+
+    }
+    class Transition
+    {
+        PosisiKarakterGame[] transition = { 
+            new PosisiKarakterGame(State.Tengkurap, State.Jongkok, Trigger.TombolW),
+            new PosisiKarakterGame(State.Jongkok, State.Tengkurap, Trigger.TombolS),
+            new PosisiKarakterGame(State.Jongkok, State.Berdiri, Trigger.TombolW),
+            new PosisiKarakterGame(State.Berdiri, State.Jongkok, Trigger.TombolS),
+            new PosisiKarakterGame(State.Berdiri, State.Terbang, Trigger.TombolW),
+            new PosisiKarakterGame(State.Terbang, State.Berdiri, Trigger.TombolS),
+            new PosisiKarakterGame(State.Terbang, State.Jongkok, Trigger.TombolX)
+
+        };
+
+        public PosisiKarakterGame NextState(PosisiKarakterGame posisi, Trigger trigger)
+        {
+            for (int i = 0; i < transition.Length; i++)
+            {
+                if (transition[i].prevState == posisi.currentState && transition[i].trigger == trigger)
+                {
+                   // Console.WriteLine(transition[i].prevState.ToString()+ transition[i].trigger.ToString() + transition[i].nextState.ToString());
+
+                    switch (trigger)
+                    {
+                        case Trigger.TombolS:
+                            Console.WriteLine("tombol arah bawah ditekan");
+                            break;
+                        case Trigger.TombolW:
+                            Console.WriteLine("tombola rah atas ditekan");
+                            break;
+                    }
+
+                    posisi.prevState = posisi.currentState;
+                    posisi.currentState = transition[i].nextState;
+                    return posisi;
+                }
+            }
+            return posisi;
+
+        }
+
+    }
+
+}
--- modul3_1302223119/Program.cs
+++ modul3_1302223119/Program.cs
@@ -1,8 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using modul4_1302223119;
+using static modul4_1302223119.PosisiKarakterGame;
+using static modul4_1302223119.Transition;
 
-Console.WriteLine("Hello, World!");
+//Soal C
+Console.WriteLine("Soal C\n");
 
 KodeBuah kodeBuah = new KodeBuah();
 
-Console.WriteLine(kodeBuah.getKodeBuah(KodeBuah.Buah.Semangka));
+Console.WriteLine(kodeBuah.getKodeBuah(KodeBuah.Buah.Melon));
+
+
+//Soal E
+
+Console.WriteLine("\n\nSoal E\n");
+
+
+Transition transition = new Transition();
+PosisiKarakterGame posisi = new PosisiKarakterGame(State.Terbang);
+
+
+transition.NextState(posisi, Trigger.TombolX);
+Console.WriteLine(posisi.currentState.ToString());
+
+transition.NextState(posisi, Trigger.TombolS);
+Console.WriteLine(posisi.currentState.ToString());
+
+transition.NextState(posisi, Trigger.TombolW);
+Console.WriteLine(posisi.currentState.ToString());
+
+transition.NextState(posisi, Trigger.TombolW);
+Console.WriteLine(posisi.currentState.ToString());
+
+transition.NextState(posisi, Trigger.TombolW);
+Console.WriteLine(posisi.currentState.ToString());
