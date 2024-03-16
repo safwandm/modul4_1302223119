1ef26ed8164989d76c85079f65e253b2db6e4f17
shuabruh <shuabruh@telkomuniversity.ac.id>
2024-03-16 07:56:09 +0700

    menambahkan class kodeBuah

---
+++ modul3_1302223119/KodeBuah.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302223119
+{
+    internal class KodeBuah
+    {
+        public enum Buah { Apel, Aprikot, Alpukat, Pisang, Paprika, Blackberry, Ceri, Kelapa, Jagung, Kurma, Durian, Anggur, Melon, Semangka };
+
+        public string getKodeBuah(Buah buah)
+        {
+            string[] kode = { "A00", "B00", "C00", "D00", "E00", "F00", "G00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00"};
+            return kode[(int)buah];
+        }
+    }
+}
---
+++ modul3_1302223119/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using modul4_1302223119;
+
+Console.WriteLine("Hello, World!");
+
+KodeBuah kodeBuah = new KodeBuah();
+
+Console.WriteLine(kodeBuah.getKodeBuah(KodeBuah.Buah.Semangka));
